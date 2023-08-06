747dbf9b8e452afdc568cc7d9051a9d31decd2ce
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-08-06 19:58:08 -0300

    criando sqlalchemy model

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/crudapi'
 
 db = SQLAlchemy(app)
+app.app_context().push()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))   
