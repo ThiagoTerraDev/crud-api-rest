bef54e527b0ce813ebd0e112cddab47f2a765f4d
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-08-06 21:25:03 -0300

    rota para consultar todos os usuarios cadastrados implementada

--- app.py
+++ app.py
@@ -13,4 +13,28 @@ app.app_context().push()
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     nome = db.Column(db.String(50))
-    email = db.Column(db.String(100))   
+    email = db.Column(db.String(100))
+    
+    def to_json(self):
+        return {"id": self.id, "nome": self.nome, "email": self.email}
+
+
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+    
+    return gera_response(200, "usuarios", usuarios_json, "ok")
+
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+    
+    if(mensagem):
+        body["mensagem"] = mensagem
+    
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+
+
+app.run()
